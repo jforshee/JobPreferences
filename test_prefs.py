c27f8bc688834d9d8afa51d71a9249628d4d1994
James Forshee <james.forshee525@gmail.com>
2012-11-15 12:39:59 -0500

    Updated sample program with more data and comments.

--- test_prefs.py
+++ test_prefs.py
@@ -2,6 +2,13 @@
 from random import shuffle
 from ranked_prefs import assign_roles, shuffled
 
+## This is a sample program on how to use the ranked_prefs library.
+## It is an implementation of assigning roles for the team game League
+## of Legends. The names used are online tagnames of my teammates, as
+## well as their preferences on roles, in descending order. For example,
+## Garver has the preference ordering ADC > SPT > TOP > MID > JGL, which
+## is encoded as [ADC, SPT, TOP, MID, JGL].
+
 ADC, SPT, MID, TOP, JGL = range(5)
 string_reps = {
     ADC : 'Carry',
@@ -14,7 +21,10 @@ string_reps = {
 preferences = {
     'Garver' : [ADC, SPT, TOP, MID, JGL],
     'Benny' : [MID, TOP, JGL, ADC, SPT],
-    'Banda' : shuffled([ADC, SPT, TOP, MID]) + [JGL]    # The faggot only cares about not playing jungle
+    'Banda' : shuffled([ADC, SPT, TOP, MID]) + [JGL],   # The man only cares about not playing jungle -.-
+    'Vekter' : [TOP, ADC, MID, JGL, SPT],
+    'Frostee' : [ADC, JGL, MID] + shuffled([TOP, SPT]), # Only cares about ADC, Jungle, and Mid
+    'Temby' : [SPT, MID, ADC, TOP, JGL]
 }
 
 mapping = assign_roles(preferences)
